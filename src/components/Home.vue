d89c877fdc168adf12eddaab79a2d2e52c846afc
Amad Khan <amadkhan.999@gmail.com>
2019-05-10 05:25:54 +0500

    project initialized

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-container>
+    <el-row>
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
